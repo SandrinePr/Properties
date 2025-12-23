adc8aa25aaeeb63782bbb2c53946e3c761245dcf
Sandrine Prumpeler <sandrineprumpeler@gmail.com>
2025-12-23 18:48:54 +0100

    517

--- functions.php
+++ functions.php
@@ -22,8 +22,10 @@ add_filter('acf/settings/show_in_rest', '__return_true');
 // 3. FORCEER ACF VELDEN IN DE WP REST API (ook in de lijst)
 add_action('rest_api_init', function() {
     register_rest_field('property', 'acf', array(
-        'get_callback' => function($object) {
-            return get_fields($object['id']);
+        'get_callback' => function($data) {
+            $fields = get_fields($data['id']);
+            // Forceer altijd een array, ook als er geen velden zijn ingevuld
+            return is_array($fields) ? $fields : new stdClass();
         },
         'schema' => null,
     ));
